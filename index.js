a91743186f410ac81f44731e7c5bcf141684faa8
SenithNimsara <senith2002n@gmail.com>
2025-07-10 13:59:55 +0530

    testing pipeling from hello message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from your CI/CD pipeline!');
+  res.send('Hello from your updated CI/CD pipeline!');
 });
 
 app.listen(port, () => {
